d7f440e492995414988fb68a49ef4ba57086dfff
shaunoff <shutch@p3i-inc.com>
2017-06-29 17:30:48 -0400

    heroku ready

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const path = require('path')
 const express = require('express')
 
+const publicPath = path.join(__dirname, '../public')
+const port = process.env.PORT || 3000
 let app = express()
 
-const publicPath = path.join(__dirname, '../public')
+
 
 app.use(express.static(publicPath))
 
-app.listen(3000, ()=>{
-  console.log("server is up on 3000")
+app.listen(port, ()=>{
+  console.log(`server is up on ${port}`)
 })
